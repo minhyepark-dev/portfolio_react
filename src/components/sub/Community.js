40e6e282c22d7db0d39beba26c02c0f49a6d127e
minhyepark <minhyepark.dev@gmail.com>
2022-01-09 20:30:14 +0900

    add community content

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -23,36 +23,49 @@ function Community() {
                 </div>
             </div>
             <div className="inner">
-                {posts.map((item, index) => {
-                    return (
-                        <article key={index} ref={article}>
-                            <div
-                                className="question"
-                                onClick={(e) => {
-                                    if (e.currentTarget.nextSibling.classList.contains("on")) {
-                                        e.currentTarget.nextSibling.classList.remove("on");
-                                    } else {
-                                        e.currentTarget.nextSibling.classList.add("on");
-                                    }
-                                }}
-                            >
-                                <h1>{item.title}</h1>
-                                <div className="right">
-                                    <span>{item.write}</span>
-                                    <em>{item.date}</em>
-                                    {arrows ? (
-                                        <FontAwesomeIcon className="icon-arrow" icon={faChevronUp} />
-                                    ) : (
-                                        <FontAwesomeIcon className="icon-arrow" icon={faChevronDown} />
-                                    )}
+                <div className="news">
+                    <div className="box on">
+                        <h2>Q&A</h2>
+                    </div>
+                    <div className="box">
+                        <h2>FAQ</h2>
+                    </div>
+                    <div className="box">
+                        <h2>Board</h2>
+                    </div>
+                </div>
+                <div className="list">
+                    {posts.map((item, index) => {
+                        return (
+                            <article key={index} ref={article}>
+                                <div
+                                    className="question"
+                                    onClick={(e) => {
+                                        if (e.currentTarget.nextSibling.classList.contains("on")) {
+                                            e.currentTarget.nextSibling.classList.remove("on");
+                                        } else {
+                                            e.currentTarget.nextSibling.classList.add("on");
+                                        }
+                                    }}
+                                >
+                                    <h1>{item.title}</h1>
+                                    <div className="right">
+                                        <span>{item.write}</span>
+                                        <em>{item.date}</em>
+                                        {arrows ? (
+                                            <FontAwesomeIcon className="icon-arrow" icon={faChevronUp} />
+                                        ) : (
+                                            <FontAwesomeIcon className="icon-arrow" icon={faChevronDown} />
+                                        )}
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="answer">
-                                <p>{item.answer}</p>
-                            </div>
-                        </article>
-                    );
-                })}
+                                <div className="answer">
+                                    <p>{item.answer}</p>
+                                </div>
+                            </article>
+                        );
+                    })}
+                </div>
             </div>
         </main>
     );
