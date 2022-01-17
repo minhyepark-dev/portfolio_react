0caa02dab76062aec210d6663e9cb6b403ad83ac
minhyepark <minhyepark.dev@gmail.com>
2022-01-18 01:04:00 +0900

    mobile nav style작업

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,19 @@
+import { useRef } from "react";
 import { NavLink } from "react-router-dom";
 
 function Header() {
+    const body = document.querySelector("body");
     const active = {
         color: "#0d211a",
+    };
+    const nav = useRef(null);
+    const btn = useRef(null);
+    const gnbClick = (e) => {
+        //btnCall에 on이 있으면 제거, 없으면 추가
+        btn.current.classList.toggle("on");
+        body.classList.toggle("on");
+        //menuMo에 on이 있으면 제거, 없으면 추가
+        nav.current.classList.toggle("on");
     };
     return (
         <header>
@@ -44,6 +55,49 @@ function Header() {
                         </NavLink>
                     </li>
                 </ul>
+                <div className="btnCall" onClick={gnbClick} ref={btn}>
+                    <span>메뉴호출</span>
+                </div>
+                <nav className="menu-mobile" ref={nav}>
+                    <h1>
+                        <NavLink exact to="/">
+                            Doremi
+                        </NavLink>
+                        <span>WE ARE YOUR ENERGY</span>
+                    </h1>
+                    <ul id="gnb-mobile">
+                        <li>
+                            <NavLink activeStyle={active} exact to="/department">
+                                Department
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeStyle={active} exact to="/community">
+                                Community
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeStyle={active} exact to="/gallery">
+                                Gallery
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeStyle={active} exact to="/youtube">
+                                Youtube
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeStyle={active} exact to="/location">
+                                Location
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeStyle={active} exact to="/join">
+                                Join
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </header>
     );
