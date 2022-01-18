f1f76c559ecb258ef058732adfeddb33ba5e0289
minhyepark <minhyepark.dev@gmail.com>
2022-01-19 01:26:30 +0900

    community tab

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -6,14 +6,37 @@ import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 function Community() {
     const base = process.env.PUBLIC_URL;
     const [posts, setPosts] = useState([]);
-    const [arrows, setArrows] = useState(false);
     let article = useRef(null);
+    const btnTab = useRef(null);
+    const boxTab = useRef(null);
+    const [enable, setEnable] = useState(true);
+
+    const handleTab = () => {
+        const btns = btnTab.current.querySelectorAll("div");
+        const boxes = boxTab.current.querySelectorAll("section > div");
+        btns.forEach((el, index) => {
+            el.addEventListener("click", (e) => {
+                let isOn = e.currentTarget.classList.contains("on");
+                if (isOn) return;
+                if (enable) {
+                    activation(btns, index);
+                    activation(boxes, index);
+                    setEnable(false);
+                }
+            });
+        });
+    };
+
+    const activation = (arr, index) => {
+        for (let item of arr) item.classList.remove("on");
+        arr[index].classList.add("on");
+    };
 
     useEffect(() => {
         axios.get(`${base}/dbs/community.json`).then((json) => {
-            console.log(json.data.data);
             setPosts(json.data.data);
         });
+        handleTab();
     }, []);
     return (
         <main className="community">
@@ -23,49 +46,104 @@ function Community() {
                 </div>
             </div>
             <div className="inner">
-                <div className="news">
-                    <div className="box on">
+                <div className="news" ref={btnTab}>
+                    <div className="box on" onClick={handleTab}>
                         <h2>Q&A</h2>
                     </div>
-                    <div className="box">
+                    <div className="box" onClick={handleTab}>
                         <h2>FAQ</h2>
                     </div>
-                    <div className="box">
+                    <div className="box" onClick={handleTab}>
                         <h2>Board</h2>
                     </div>
                 </div>
-                <div className="list">
-                    {posts.map((item, index) => {
-                        return (
-                            <article key={index} ref={article}>
-                                <div
-                                    className="question"
-                                    onClick={(e) => {
-                                        if (e.currentTarget.nextSibling.classList.contains("on")) {
-                                            e.currentTarget.nextSibling.classList.remove("on");
-                                        } else {
-                                            e.currentTarget.nextSibling.classList.add("on");
-                                        }
-                                    }}
-                                >
-                                    <h1>{item.title}</h1>
-                                    <div className="right">
-                                        <span>{item.write}</span>
-                                        <em>{item.date}</em>
-                                        {arrows ? (
-                                            <FontAwesomeIcon className="icon-arrow" icon={faChevronUp} />
-                                        ) : (
+                <section ref={boxTab}>
+                    <div className="list on">
+                        {posts.map((item, index) => {
+                            return (
+                                <article key={index} ref={article}>
+                                    <div
+                                        className="question"
+                                        onClick={(e) => {
+                                            if (e.currentTarget.nextSibling.classList.contains("on")) {
+                                                e.currentTarget.nextSibling.classList.remove("on");
+                                            } else {
+                                                e.currentTarget.nextSibling.classList.add("on");
+                                                e.currentTarget.lastElementChild.querySelector(".off").classList.add("on");
+                                            }
+                                        }}
+                                    >
+                                        <h1>{item.title + "1"}</h1>
+                                        <div className="right">
+                                            <span>{item.write}</span>
+                                            <em>{item.date}</em>
                                             <FontAwesomeIcon className="icon-arrow" icon={faChevronDown} />
-                                        )}
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="answer">
-                                    <p>{item.answer}</p>
-                                </div>
-                            </article>
-                        );
-                    })}
-                </div>
+                                    <div className="answer">
+                                        <p>{item.answer}</p>
+                                    </div>
+                                </article>
+                            );
+                        })}
+                    </div>
+                    <div className="list">
+                        {posts.map((item, index) => {
+                            return (
+                                <article key={index} ref={article}>
+                                    <div
+                                        className="question"
+                                        onClick={(e) => {
+                                            if (e.currentTarget.nextSibling.classList.contains("on")) {
+                                                e.currentTarget.nextSibling.classList.remove("on");
+                                            } else {
+                                                e.currentTarget.nextSibling.classList.add("on");
+                                            }
+                                        }}
+                                    >
+                                        <h1>{item.title + "2"}</h1>
+                                        <div className="right">
+                                            <span>{item.write}</span>
+                                            <em>{item.date}</em>
+                                            <FontAwesomeIcon className="icon-arrow" icon={faChevronDown} />
+                                        </div>
+                                    </div>
+                                    <div className="answer">
+                                        <p>{item.answer}</p>
+                                    </div>
+                                </article>
+                            );
+                        })}
+                    </div>
+                    <div className="list">
+                        {posts.map((item, index) => {
+                            return (
+                                <article key={index} ref={article}>
+                                    <div
+                                        className="question"
+                                        onClick={(e) => {
+                                            if (e.currentTarget.nextSibling.classList.contains("on")) {
+                                                e.currentTarget.nextSibling.classList.remove("on");
+                                            } else {
+                                                e.currentTarget.nextSibling.classList.add("on");
+                                            }
+                                        }}
+                                    >
+                                        <h1>{item.title + "3"}</h1>
+                                        <div className="right">
+                                            <span>{item.write}</span>
+                                            <em>{item.date}</em>
+                                            <FontAwesomeIcon className="icon-arrow" icon={faChevronDown} />
+                                        </div>
+                                    </div>
+                                    <div className="answer">
+                                        <p>{item.answer}</p>
+                                    </div>
+                                </article>
+                            );
+                        })}
+                    </div>
+                </section>
             </div>
         </main>
     );
