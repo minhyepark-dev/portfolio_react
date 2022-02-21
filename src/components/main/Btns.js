5be816845157c91f49d49e7dfd37ac98e72d76d9
minhyepark <minhyepark.dev@gmail.com>
2022-02-21 17:34:01 +0900

    버튼 class on 추가

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,7 +1,7 @@
 function Btns(props) {
     return (
         <ul id="btns">
-            <li onClick={() => props.getIndex(0)}></li>
+            <li className="on" onClick={() => props.getIndex(0)}></li>
             <li onClick={() => props.getIndex(1)}></li>
             <li onClick={() => props.getIndex(2)}></li>
             <li onClick={() => props.getIndex(3)}></li>
