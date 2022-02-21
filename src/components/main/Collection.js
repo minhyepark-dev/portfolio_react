a709effc1a88a8f58e6b608a1aa76ec607ae79ad
minhyepark <minhyepark.dev@gmail.com>
2022-02-21 17:33:44 +0900

    메인 하단 콜렉션 이미지 수정

--- src/components/main/Collection.js
+++ src/components/main/Collection.js
@@ -1,23 +1,22 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setGallery } from "../../redux/actions";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 function Collection() {
-    const gallery = useSelector((state) => state);
-    const dispatch = useDispatch();
-    const galData = gallery.galleryReducer.gallery;
+    const [items, setItems] = useState([]);
 
     const base = "https://www.flickr.com/services/rest/?";
     const method = "flickr.galleries.getPhotos";
     const api_key = "9e85f04dacb4a41b834ddd33463ef00e";
     const per_page = 6;
-    const url2 = `${base}method=${method}&api_key=${api_key}&per_page=${per_page}&format=json&nojsoncallback=1&gallery_id=72157720230522725`;
+    const url = `${base}method=${method}&api_key=${api_key}&per_page=${per_page}&format=json&nojsoncallback=1&extras=views,owner_name&gallery_id=72157720230522725`;
     const fetchGallery = async () => {
-        const response = await axios.get(url2).catch((err) => {
-            console.error(err);
+        await axios.get(url).then((json) => {
+            const photo = json.data.photos.photo;
+            console.log(photo);
+            setItems(photo);
         });
-        dispatch(setGallery(response.data.photos.photo));
     };
     useEffect(() => {
         fetchGallery();
@@ -26,35 +25,59 @@ function Collection() {
         <main className="collection myScroll">
             <div className="inner">
                 <div className="img-wrap">
-                    {galData
+                    {items
                         .filter((_, index) => index === 0)
                         .map((item, index) => {
                             const imgSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_z.jpg`;
                             return (
-                                <div className="img" key={index}>
+                                <div className="img big" key={index}>
                                     <img src={imgSrc} />
+                                    <div className="img-text">
+                                        <h3>{item.title}</h3>
+                                        <p>{item.ownername}</p>
+                                        <p>
+                                            <FontAwesomeIcon icon={faEye} className="icon-views" />
+                                            {item.views}
+                                        </p>
+                                    </div>
                                 </div>
                             );
                         })}
                     <div className="img">
-                        {galData
+                        {items
                             .filter((_, index) => index > 0 && index <= 4)
                             .map((item, index) => {
                                 const imgSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_w.jpg`;
                                 return (
                                     <div className="img-inner" key={index}>
                                         <img src={imgSrc} />
+                                        <div className="img-text">
+                                            <h3>{item.title}</h3>
+                                            <p>{item.ownername}</p>
+                                            <p>
+                                                <FontAwesomeIcon icon={faEye} className="icon-views" />
+                                                {item.views}
+                                            </p>
+                                        </div>
                                     </div>
                                 );
                             })}
                     </div>
-                    {galData
+                    {items
                         .filter((_, index) => index === 5)
                         .map((item, index) => {
                             const imgSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_z.jpg`;
                             return (
-                                <div className="img" key={index}>
+                                <div className="img big" key={index}>
                                     <img src={imgSrc} />
+                                    <div className="img-text">
+                                        <h3>{item.title}</h3>
+                                        <p>{item.ownername}</p>
+                                        <p>
+                                            <FontAwesomeIcon icon={faEye} className="icon-views" />
+                                            {item.views}
+                                        </p>
+                                    </div>
                                 </div>
                             );
                         })}
