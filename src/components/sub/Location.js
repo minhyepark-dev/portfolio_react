26769a11e781f8d508dbd69abf32a548259a1fc5
minhyepark <minhyepark.dev@gmail.com>
2025-03-14 16:28:33 +0900

    edit locaiton file

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,134 +1,335 @@
 import { useEffect, useRef, useState } from 'react';
-
-const kakaoMapKey = process.env.REACT_APP_KAKAO_MAP_API_KEY;
-
-function loadKakaoMap() {
-  return new Promise((resolve, reject) => {
-    if (window.kakao && window.kakao.maps) {
-      resolve(); // 이미 로드된 경우 바로 resolve
-    } else {
-      const script = document.createElement('script');
-      script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoMapKey}&autoload=false`;
-      script.async = true;
-      script.onload = () => {
-        if (window.kakao && window.kakao.maps) {
-          resolve(); // 정상적으로 로드되면 resolve
-        } else {
-          reject(new Error('Kakao Maps API failed to load'));
-        }
-      };
-      script.onerror = (error) => reject(error);
-      document.head.appendChild(script);
-    }
-  });
-}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPhoneAlt,
+  faMapMarkerAlt,
+  faEnvelope,
+  faClock,
+  faTrafficLight,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Location() {
-  const container = useRef(null); // 지도 컨테이너
-  const btnBranch = useRef(null); // 버튼들
-  const [map, setMap] = useState(null); // 지도 상태
-  const [index, setIndex] = useState(0); // 현재 선택된 지점 인덱스
-  const [mapLoaded, setMapLoaded] = useState(false); // 지도 로드 상태
-  const [mapInfo, setMapInfo] = useState([]); // 지도에 표시할 지점 정보
+  const { kakao } = window;
+  const container = useRef(null);
+  const btnBranch = useRef(null);
+  const [map, setMap] = useState(null);
+  const [index, setIndex] = useState(0);
+  const [toggle, setToggle] = useState(false);
+  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
 
+  // const info = [
+  //   {
+  //     title: '본점',
+  //     latlng: new kakao.maps.LatLng(37.51276949548283, 127.05883923964538),
+  //     imgSrc: process.env.PUBLIC_URL + '/img/pin.png',
+  //     imgSize: new kakao.maps.Size(50, 50),
+  //     imgPos: { offset: new kakao.maps.Point(25, 50) },
+  //   },
+  //   {
+  //     title: '지점1',
+  //     latlng: new kakao.maps.LatLng(37.57975318699628, 126.97709192859216),
+  //     imgSrc: process.env.PUBLIC_URL + '/img/pin2.png',
+  //     imgSize: new kakao.maps.Size(50, 50),
+  //     imgPos: { offset: new kakao.maps.Point(25, 55) },
+  //   },
+  //   {
+  //     title: '지점2',
+  //     latlng: new kakao.maps.LatLng(35.15317690680852, 129.11898444686017),
+  //     imgSrc: process.env.PUBLIC_URL + '/img/pin3.png',
+  //     imgSize: new kakao.maps.Size(50, 50),
+  //     imgPos: { offset: new kakao.maps.Point(25, 50) },
+  //   },
+  // ];
+
+  const info = [
+    {
+      title: '본점',
+      latlng: { lat: 37.51276949548283, lng: 127.05883923964538 },
+      imgSrc: process.env.PUBLIC_URL + '/img/pin.png',
+      imgSize: { width: 50, height: 50 },
+      imgPos: { offsetX: 25, offsetY: 50 },
+    },
+    {
+      title: '지점1',
+      latlng: { lat: 37.57975318699628, lng: 126.97709192859216 },
+      imgSrc: process.env.PUBLIC_URL + '/img/pin2.png',
+      imgSize: { width: 50, height: 50 },
+      imgPos: { offsetX: 25, offsetY: 55 },
+    },
+    {
+      title: '지점2',
+      latlng: { lat: 35.15317690680852, lng: 129.11898444686017 },
+      imgSrc: process.env.PUBLIC_URL + '/img/pin3.png',
+      imgSize: { width: 50, height: 50 },
+      imgPos: { offsetX: 25, offsetY: 50 },
+    },
+  ];
+
+  // ✅ Kakao 지도 스크립트 동적 로드
   useEffect(() => {
-    async function initializeMap() {
-      try {
-        await loadKakaoMap(); // KakaoMap 로드 대기
-        if (window.kakao && window.kakao.maps) {
-          // 지도 정보 설정
-          setMapInfo([
-            {
-              title: '본점',
-              latlng: new window.kakao.maps.LatLng(
-                37.51276949548283,
-                127.05883923964538
-              ),
-              imgSrc: process.env.PUBLIC_URL + '/img/pin.png',
-              imgSize: new window.kakao.maps.Size(50, 50),
-              imgPos: { offset: new window.kakao.maps.Point(25, 50) },
-            },
-            {
-              title: '지점1',
-              latlng: new window.kakao.maps.LatLng(
-                37.57975318699628,
-                126.97709192859216
-              ),
-              imgSrc: process.env.PUBLIC_URL + '/img/pin2.png',
-              imgSize: new window.kakao.maps.Size(50, 50),
-              imgPos: { offset: new window.kakao.maps.Point(25, 55) },
-            },
-            {
-              title: '지점2',
-              latlng: new window.kakao.maps.LatLng(
-                35.15317690680852,
-                129.11898444686017
-              ),
-              imgSrc: process.env.PUBLIC_URL + '/img/pin3.png',
-              imgSize: new window.kakao.maps.Size(50, 50),
-              imgPos: { offset: new window.kakao.maps.Point(25, 50) },
-            },
-          ]);
-          setMapLoaded(true); // 로드 완료 후 mapLoaded true로 설정
-        } else {
-          throw new Error('Kakao Maps API is not loaded properly.');
-        }
-      } catch (error) {
-        console.error('Failed to load Kakao Maps API:', error);
-      }
-    }
-
-    initializeMap();
-  }, []); // 한 번만 실행되도록 빈 배열 전달
-
-  useEffect(() => {
-    if (!mapLoaded || !container.current || mapInfo.length === 0) return;
-
-    // 지도 옵션 설정
-    const options = {
-      center: mapInfo[index].latlng, // 선택된 지점의 좌표로 초기화
-      level: 3, // 지도 확대 레벨
-    };
-
-    // 지도 인스턴스 생성
-    const newMap = new window.kakao.maps.Map(container.current, options);
-    setMap(newMap);
-
-    // 마커 생성
-    new window.kakao.maps.Marker({
-      map: newMap,
-      position: mapInfo[index].latlng,
-      title: mapInfo[index].title,
-      image: new window.kakao.maps.MarkerImage(
-        mapInfo[index].imgSrc,
-        mapInfo[index].imgSize,
-        mapInfo[index].imgPos
-      ),
-    });
-
-    // 지도 설정
-    newMap.setCenter(mapInfo[index].latlng);
-    newMap.setZoomable(true);
-    newMap.setDraggable(true);
-
-    // 버튼 활성화 상태 변경
-    for (const btn of btnBranch.current.children) btn.classList.remove('on');
-    btnBranch.current.children[index].classList.add('on');
-
-    // 윈도우 크기 변경 시 지도 중앙 재설정
-    const mapSet = () => newMap.setCenter(mapInfo[index].latlng);
-    window.addEventListener('resize', mapSet);
+    const script = document.createElement('script');
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_API_KEY}&autoload=false`;
+    script.async = true;
+    script.onload = () => setIsScriptLoaded(true);
+    document.head.appendChild(script);
 
     return () => {
-      window.removeEventListener('resize', mapSet);
-      container.current.innerHTML = ''; // 컴포넌트 언마운트 시 맵 리셋
+      document.head.removeChild(script);
     };
-  }, [index, mapLoaded, mapInfo]); // `index`와 `mapInfo` 변경 시 실행
+  }, []);
 
+  // const [mapInfo, setMapInfo] = useState(info);
+  // ✅ 스크립트 로드 후 지도 생성
+  useEffect(() => {
+    if (!isScriptLoaded || !window.kakao) return;
+
+    window.kakao.maps.load(() => {
+      const kakao = window.kakao;
+      const options = {
+        center: new kakao.maps.LatLng(
+          info[index].latlng.lat,
+          info[index].latlng.lng
+        ),
+        level: 3,
+      };
+      const newMap = new kakao.maps.Map(container.current, options);
+      setMap(newMap);
+
+      new kakao.maps.Marker({
+        map: newMap,
+        position: new kakao.maps.LatLng(
+          info[index].latlng.lat,
+          info[index].latlng.lng
+        ),
+        title: info[index].title,
+        image: new kakao.maps.MarkerImage(
+          info[index].imgSrc,
+          new kakao.maps.Size(
+            info[index].imgSize.width,
+            info[index].imgSize.height
+          ),
+          {
+            offset: new kakao.maps.Point(
+              info[index].imgPos.offsetX,
+              info[index].imgPos.offsetY
+            ),
+          }
+        ),
+      });
+
+      newMap.setCenter(
+        new kakao.maps.LatLng(info[index].latlng.lat, info[index].latlng.lng)
+      );
+
+      // 버튼 스타일 변경
+      if (btnBranch.current) {
+        for (const btn of btnBranch.current.children)
+          btn.classList.remove('on');
+        btnBranch.current.children[index].classList.add('on');
+      }
+
+      // 창 크기 조정 시 지도 중심 변경
+      const mapSet = () =>
+        newMap.setCenter(
+          new kakao.maps.LatLng(info[index].latlng.lat, info[index].latlng.lng)
+        );
+      window.addEventListener('resize', mapSet);
+
+      return () => {
+        window.removeEventListener('resize', mapSet);
+        container.current.innerHTML = '';
+      };
+    });
+  }, [isScriptLoaded, index]);
+
+  function setMapType(maptype) {
+    var roadmapControl = document.getElementById('btnRoadmap');
+    var skyviewControl = document.getElementById('btnSkyview');
+    if (maptype === 'roadmap') {
+      map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
+      roadmapControl.className = 'selected_btn';
+      skyviewControl.className = 'btn';
+    } else {
+      map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+      skyviewControl.className = 'selected_btn';
+      roadmapControl.className = 'btn';
+    }
+  }
+  // useEffect(() => {
+  //   const options = {
+  //     center: mapInfo[index].latlng,
+  //     level: 3,
+  //   };
+  //   const map = new kakao.maps.Map(container.current, options);
+  //   setMap(map);
+  //   new kakao.maps.Marker({
+  //     map: map,
+  //     position: mapInfo[index].latlng,
+  //     title: mapInfo[index].title,
+  //     image: new kakao.maps.MarkerImage(
+  //       mapInfo[index].imgSrc,
+  //       mapInfo[index].imgSize,
+  //       mapInfo[index].imgPos
+  //     ), // 마커 이미지
+  //   });
+  //   map.setCenter(mapInfo[index].latlng);
+
+  //   map.setZoomable(true);
+  //   map.setDraggable(true);
+
+  //   for (const btn of btnBranch.current.children) btn.classList.remove('on');
+  //   btnBranch.current.children[index].classList.add('on');
+
+  //   const mapSet = () => map.setCenter(mapInfo[index].latlng);
+  //   window.addEventListener('resize', mapSet);
+  //   return () => {
+  //     window.removeEventListener('resize', mapSet);
+  //     container.current.innerHTML = '';
+  //   };
+  // }, [index]);
   return (
-    <main id="main" className="location">
-      <div className="map-wrap">
-        <div id="map" ref={container}></div>
+    <main className="location">
+      <div className="sub-visual">
+        <div className="inner">
+          <h1>Location</h1>
+        </div>
+      </div>
+      <div className="inner">
+        <div className="contact">
+          <h2>Our Contact</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, aut.{' '}
+            <br /> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </p>
+          <div className="contact_box">
+            <div className="box">
+              <FontAwesomeIcon className="icon" icon={faMapMarkerAlt} />
+              <p>Address</p>
+              <p>
+                12 Van Dyke St. Brooklyn. <br /> NY 11231
+              </p>
+            </div>
+            <div className="box">
+              <FontAwesomeIcon className="icon" icon={faPhoneAlt} />
+              <p>Phone</p>
+              <p>
+                1(800)123-4567 <br /> 1(800)123-7897
+              </p>
+            </div>
+            <div className="box">
+              <FontAwesomeIcon className="icon" icon={faEnvelope} />
+              <p>Mail</p>
+              <p>
+                hello@doremi.com <br /> service@doremi.com
+              </p>
+            </div>
+            <div className="box">
+              <FontAwesomeIcon className="icon" icon={faClock} />
+              <p>Hours</p>
+              <p>
+                Monday - Friday <br /> 09:00am - 18:00pm
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="map-wrap">
+          <ul className="branch" ref={btnBranch}>
+            <li
+              onClick={() => {
+                setIndex(0);
+              }}
+            >
+              <FontAwesomeIcon className="icon" icon={faMapMarkerAlt} />
+              <div className="map-text">
+                <p>headquarter</p>
+                <p>
+                  Lorem ipsum dolor sit. <br />
+                  Lorem, ipsum dolor.
+                </p>
+              </div>
+            </li>
+            <li
+              onClick={() => {
+                setIndex(1);
+              }}
+            >
+              <FontAwesomeIcon className="icon" icon={faMapMarkerAlt} />
+              <div className="map-text">
+                <p>Branch 1</p>
+                <p>
+                  Lorem ipsum dolor sit. <br />
+                  Lorem, ipsum dolor.
+                </p>
+              </div>
+            </li>
+            <li
+              onClick={() => {
+                setIndex(2);
+              }}
+            >
+              <FontAwesomeIcon className="icon" icon={faMapMarkerAlt} />
+              <div className="map-text">
+                <p>Branch 2</p>
+                <p>
+                  Lorem ipsum dolor sit. <br />
+                  Lorem, ipsum dolor.
+                </p>
+              </div>
+            </li>
+          </ul>
+          <div className="map-inner">
+            <div className="custom_typecontrol radius_border">
+              <span
+                id="btnRoadmap"
+                className="selected_btn"
+                onClick={() => {
+                  setMapType('roadmap');
+                }}
+              >
+                Map
+              </span>
+              <span
+                id="btnSkyview"
+                className="btn"
+                onClick={() => {
+                  setMapType('skyview');
+                }}
+              >
+                Sky View
+              </span>
+            </div>
+            <ul className="traffic">
+              {toggle ? (
+                <li
+                  onClick={() => {
+                    map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+                    setToggle(!toggle);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    className="traffic-icon"
+                    icon={faTrafficLight}
+                  />
+                  Off the traffic
+                </li>
+              ) : (
+                <li
+                  onClick={() => {
+                    map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+                    setToggle(!toggle);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    className="traffic-icon"
+                    icon={faTrafficLight}
+                  />
+                  On the traffic
+                </li>
+              )}
+            </ul>
+            <div id="map" ref={container}></div>
+          </div>
+        </div>
       </div>
     </main>
   );
